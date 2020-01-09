2dd7a1e633ca6c1914108e51c8f3f5abd275079d
abberdin <abdurahim.khudoyberdiev@gmail.com>
2020-01-09 21:35:25 +0500

    Updated: MyAppData comment

--- AlifQuotesAPI/Controllers/MyAppData.cs
+++ AlifQuotesAPI/Controllers/MyAppData.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace AlifQuotesAPI.Controllers
 {
     /// <summary>
+    /// For preserve user data and app state between requests.
+    /// Used Dependency Injection to make data available to all users:
     /// Based on:
     /// https://docs.microsoft.com/en-us/aspnet/core/fundamentals/app-state?view=aspnetcore-3.1#dependency-injection
     /// </summary>
